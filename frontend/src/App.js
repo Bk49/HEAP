55cad5d0fc395d204d24617456a0e928d239ba64
Bk49 <ericngyongwei@gmail.com>
2023-05-05 14:41:12 +0800

    Comment for fun

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ const App = () => {
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
+                    // Comment for fun
                     className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
