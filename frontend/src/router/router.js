77fcefdc2584f6283dea4b0eff6f08b20c0d3dbe
Bk49 <ericngyongwei@gmail.com>
2023-05-22 16:38:00 +0800

    Include login and register page in router

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,11 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Error from "./pages/Error";
+import Home from "../pages/Home";
+import Error from "../pages/Error";
+import PublicRoute from "./routes/PublicRoute";
+import Login from "../pages/user/Login";
+import RegistrationOne from "../pages/user/RegistrationOne";
 
 export default createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
+        element: (
+            <PublicRoute>
+                <Home />
+            </PublicRoute>
+        ),
         errorElement: <Error />,
+    },
+    {
+        path: "login",
+        element: (
+            <PublicRoute>
+                <Login />
+            </PublicRoute>
+        ),
+    },
+    {
+        path: "register",
+        element: (
+            <PublicRoute>
+                <RegistrationOne />
+            </PublicRoute>
+        ),
     },
 ]);
