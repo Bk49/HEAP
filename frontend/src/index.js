92d6cde836b325ad82a6754ddebdb8d726701cdb
Bk49 <ericngyongwei@gmail.com>
2023-05-22 16:32:43 +0800

    Import Roboto fonts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,10 @@ import { RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./css/index.css"
 import router from "./router/router";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
