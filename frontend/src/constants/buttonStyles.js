3bb862a29f150e805bc01bbd774d62d4c4152afc
Bk49 <ericngyongwei@gmail.com>
2023-05-23 15:22:11 +0800

    Create constant button components styles in separate file

---
+++ frontend/src/constants/buttonStyles.js
@@ -0,0 +1,11 @@
+export const smallButtonStyles = {
+    primary: { backgroundColor: "#163172", hoverColor: "#0E1E43" },
+    secondary: { backgroundColor: "#43ACD9", hoverColor: "#398DB0" },
+    error: { backgroundColor: "#F44336", hoverColor: "#D32F2F" },
+    default: { backgroundColor: "#9E9E9E", hoverColor: "#616161" },
+};
+
+export const bigButtonStyles = {
+    primary: { backgroundColor: "#163172", hoverColor: "#0E1E43" },
+    secondary: { backgroundColor: "#43ACD9", hoverColor: "#398DB0" },
+};
