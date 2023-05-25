707eed3305702e264081fa7880bfbcefe7779c2d
Bk49 <ericngyongwei@gmail.com>
2023-05-25 16:06:50 +0800

    Modify RegistrationTwo.js to add form states and error handling

--- frontend/src/pages/user/RegistrationTwo.js
+++ frontend/src/pages/user/RegistrationTwo.js
@@ -1,66 +1,95 @@
 import HeadingOne from "../../components/common/heading/HeadingOne";
 import RegisterIllustration from "../../assets/illustrations/user/registration-illustration-2.jpg";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import TextField from "../../components/common/form/TextField";
-import MUITextField from "@mui/material/TextField";
 import HeadingTwo from "../../components/common/heading/HeadingTwo";
-import Select from "@mui/material/Select";
 import { businessType, cuisineType } from "../../constants/dropdown-choices";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
 import Checkbox from "../../components/common/form/Checkbox";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Divider from "@mui/material/Divider";
-import Button from "@mui/material/Button";
 import FieldsColumn from "../../components/common/form/FieldsColumn";
 import TextArea from "../../components/common/form/TextArea";
 import SingleItemDropdown from "../../components/common/form/SingleItemDropdown";
 import SubmitFormGroup from "../../components/common/form/SubmitFormGroup";
-import SmallButton from "../../components/common/button/SmallButton";
+import { FormProvider, useForm } from "react-hook-form";
+import { useNavigate, useLocation } from "react-router-dom";
+import MultiItemDropdown from "../../components/common/form/MultiItemDropdown";
 
 const RegistrationTwo = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const formMethods = useForm();
+    const { setValue } = formMethods;
+
+    useEffect(() => {
+        if (!location.state) {
+            return navigate("register", { state: { error: "No state" } });
+        }
+        const { email, password } = location.state;
+        console.log(email, password);
+        setValue("email", email);
+        setValue("password", password);
+    }, [location.state, setValue, navigate]);
+
     return (
         <Fragment>
             <HeadingOne divider={true}>Register: Business Details</HeadingOne>
-            <Grid container spacing={2}>
-                <Grid xs={6}>
-                    <HeadingTwo>Basic Details</HeadingTwo>
-                    <FieldsColumn>
-                        <TextField label="Name of F&B Business" size="large" />
-                        <TextArea label="Store Address" />
-                        <TextField label="Postal Code" size="large" />
-                    </FieldsColumn>
-                    <HeadingTwo>Cuisine Choices</HeadingTwo>
-                    <FieldsColumn>
-                        <SingleItemDropdown
-                            label="Type of F&B Business"
-                            size="large"
-                            choices={businessType}
+            <FormProvider {...formMethods}>
+                <Grid container spacing={2}>
+                    <Grid xs={6}>
+                        <HeadingTwo>Basic Details</HeadingTwo>
+                        <FieldsColumn>
+                            <TextField
+                                rules={{ required: true }}
+                                name="businessName"
+                                label="Name of F&B Business"
+                                size="large"
+                            />
+                            <TextArea
+                                rules={{ required: true }}
+                                name="storeAddress"
+                                label="Store Address"
+                            />
+                            <TextField
+                                rules={{ require: true }}
+                                name="postalCode"
+                                label="Postal Code"
+                                type="number"
+                                size="large"
+                            />
+                        </FieldsColumn>
+                        <HeadingTwo>Cuisine Choices</HeadingTwo>
+                        <FieldsColumn>
+                            <SingleItemDropdown
+                                name="businessType"
+                                rules={{ require: true }}
+                                label="Type of F&B Business"
+                                size="large"
+                                choices={businessType}
+                            />
+                            <MultiItemDropdown
+                                name="cusineType"
+                                rules={{ require: true }}
+                                label="Type of Cuisine"
+                                size="large"
+                                choices={cuisineType}
+                            />
+                            <Checkbox label="Fusion Cuisine?" />
+                        </FieldsColumn>
+                    </Grid>
+                    <Grid xs={6}>
+                        <img
+                            style={{ width: "100%" }}
+                            src={RegisterIllustration}
+                            alt="Register Illustration"
                         />
-                        {/* <FormControl variant="filled" sx={{ width: "40rem" }}>
-                        <InputLabel>Type of Cuisine</InputLabel>
-                        <Select multiple>
-                            {cuisineType.map(({ text, value }) => (
-                                <MenuItem key={text} value={value}>
-                                    {text}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl> */}
-                        <Checkbox label="Fusion Cuisine?" />
-                    </FieldsColumn>
+                    </Grid>
                 </Grid>
-                <Grid xs={6}>
-                    <img
-                        style={{ width: "100%" }}
-                        src={RegisterIllustration}
-                        alt="Register Illustration"
-                    />
-                </Grid>
-            </Grid>
-            <SubmitFormGroup submitText="Register Now"/>
+                <SubmitFormGroup
+                    submitErrorText="Registration unsuccessful, please check your input"
+                    onSubmit={(data) => console.log(data)}
+                    submitText="Register Now"
+                />
+            </FormProvider>
         </Fragment>
     );
 };
