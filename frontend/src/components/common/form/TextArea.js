ab0ec4bda0172aec737b68d338eec47c95fdb85d
Bk49 <ericngyongwei@gmail.com>
2023-05-25 16:02:39 +0800

    Modify TextArea component to include form states and error handling

--- frontend/src/components/common/form/TextArea.js
+++ frontend/src/components/common/form/TextArea.js
@@ -1,13 +1,31 @@
 import TextField from "@mui/material/TextField";
+import { useFormContext, Controller } from "react-hook-form";
+import { generateRules } from "../../../functions/generateRules";
 
-const TextArea = ({ label = "Text Area" }) => {
+const TextArea = ({ rules = {}, name = "textarea", label = "Text Area" }) => {
+    const {
+        control,
+        formState: { errors },
+    } = useFormContext();
+    const error = errors[name];
+
     return (
-        <TextField
-            label={label}
-            sx={{ width: "40rem" }}
-            rows={7}
-            multiline
-            variant="filled"
+        <Controller
+            name={name}
+            control={control}
+            rules={generateRules({ name: label, ...rules })}
+            render={({ field }) => (
+                <TextField
+                    {...field}
+                    error={error}
+                    helperText={error ? error.message : ""}
+                    label={label}
+                    sx={{ width: "40rem" }}
+                    rows={7}
+                    multiline
+                    variant="filled"
+                />
+            )}
         />
     );
 };
