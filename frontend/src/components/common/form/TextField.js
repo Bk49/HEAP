d41c2ccfd8513e05c6b79d6bb50cb383c4a896f5
Bk49 <ericngyongwei@gmail.com>
2023-05-25 16:02:54 +0800

    Modify TextField component to include form states and error handling

--- frontend/src/components/common/form/TextField.js
+++ frontend/src/components/common/form/TextField.js
@@ -1,25 +1,47 @@
 import MUITextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
+import { useFormContext, Controller } from "react-hook-form";
+import { generateRules } from "../../../functions/generateRules";
 
 const TextField = ({
+    rules = {},
+    name = "textfield",
     icon,
     label = "Label Text",
     type = "text",
     size = "small",
 }) => {
+    const {
+        control,
+        formState: { errors },
+    } = useFormContext();
+    const error = errors[name];
+
     return (
-        <MUITextField
-            label={label}
-            type={type}
-            InputProps={
-                icon && {
-                    startAdornment: (
-                        <InputAdornment position="start">{icon}</InputAdornment>
-                    ),
-                }
-            }
-            sx={{ width: size === "small" ? "20rem" : "40rem" }}
-            variant="filled"
+        <Controller
+            name={name}
+            control={control}
+            rules={generateRules({ name: label, ...rules })}
+            render={({ field }) => (
+                <MUITextField
+                    {...field}
+                    error={error}
+                    helperText={error ? error.message : ""}
+                    label={label}
+                    type={type}
+                    InputProps={
+                        icon && {
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    {icon}
+                                </InputAdornment>
+                            ),
+                        }
+                    }
+                    sx={{ width: size === "small" ? "20rem" : "40rem" }}
+                    variant="filled"
+                />
+            )}
         />
     );
 };
