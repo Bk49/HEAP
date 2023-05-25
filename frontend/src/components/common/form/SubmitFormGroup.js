da90ee7103eb93bd5cd36b95dd95a6d3583f156c
Bk49 <ericngyongwei@gmail.com>
2023-05-25 16:02:13 +0800

    Modify SubmitFormGroup to handle errors and for submissions

--- frontend/src/components/common/form/SubmitFormGroup.js
+++ frontend/src/components/common/form/SubmitFormGroup.js
@@ -1,8 +1,26 @@
 import Divider from "@mui/material/Divider";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import SmallButton from "../button/SmallButton";
+import { useFormContext } from "react-hook-form";
+import { useSnackbar } from "notistack";
+import { enqueueError } from "../../../functions/formHandling";
 
-const SubmitFormGroup = ({ submitText = "Confirm Changes" }) => {
+const SubmitFormGroup = ({
+    onSubmit = (data) => console.log(data),
+    submitErrorText = "Form submission unsuccessful",
+    submitText = "Confirm Changes",
+}) => {
+    const {
+        handleSubmit,
+        formState: { errors },
+    } = useFormContext();
+    const { enqueueSnackbar } = useSnackbar();
+    const [update, setUpdate] = useState(0);
+
+    useEffect(() => {
+        enqueueError(submitErrorText, errors, enqueueSnackbar);
+    }, [enqueueSnackbar, errors, submitErrorText, update]);
+
     return (
         <Fragment>
             <Divider sx={{ margin: "1rem 0" }} />
@@ -16,7 +34,15 @@ const SubmitFormGroup = ({ submitText = "Confirm Changes" }) => {
                 }}
             >
                 <SmallButton type="default">Return</SmallButton>
-                <SmallButton type="primary">{submitText}</SmallButton>
+                <SmallButton
+                    onClick={() => {
+                        handleSubmit(onSubmit)();
+                        setUpdate((prev) => prev + 1);
+                    }}
+                    type="primary"
+                >
+                    {submitText}
+                </SmallButton>
             </div>
         </Fragment>
     );
