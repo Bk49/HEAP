3b22ef0103eb807bef25c4dee1145166d8be2ad6
Bk49 <ericngyongwei@gmail.com>
2023-05-25 15:33:04 +0800

    Modify SingleItemDropdown component to include form states and error handling

--- frontend/src/components/common/form/SingleItemDropdown.js
+++ frontend/src/components/common/form/SingleItemDropdown.js
@@ -2,26 +2,48 @@ import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import { useFormContext, Controller } from "react-hook-form";
+import { generateRules } from "../../../functions/generateRules";
+import { FormHelperText } from "@mui/material";
 
 const SingleItemDropdown = ({
+    rules = {},
+    name = "singledropdown",
     label = "Single Dropdown",
     size = "small",
     choices = [{ text: "No Choices", value: "" }],
 }) => {
+    const {
+        control,
+        formState: { errors },
+    } = useFormContext();
+    const error = errors[name];
+
     return (
-        <FormControl
-            variant="filled"
-            sx={{ width: size === "small" ? "20rem" : "40rem" }}
-        >
-            <InputLabel>{label}</InputLabel>
-            <Select>
-                {choices.map(({ text, value }) => (
-                    <MenuItem key={text} value={value}>
-                        {value === "" ? <em>{text}</em> : text}
-                    </MenuItem>
-                ))}
-            </Select>
-        </FormControl>
+        <Controller
+            control={control}
+            name={name}
+            rules={generateRules({ name: label, ...rules })}
+            render={({ field }) => (
+                <FormControl
+                    error={error}
+                    variant="filled"
+                    sx={{ width: size === "small" ? "20rem" : "40rem" }}
+                >
+                    <InputLabel>{label}</InputLabel>
+                    <Select {...field} value={field.value || ""}>
+                        {choices.map(({ text, value }) => (
+                            <MenuItem key={text} value={value}>
+                                {value === "" ? <em>{text}</em> : text}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                    {error && error.message && (
+                        <FormHelperText>{error.message}</FormHelperText>
+                    )}
+                </FormControl>
+            )}
+        />
     );
 };
 
