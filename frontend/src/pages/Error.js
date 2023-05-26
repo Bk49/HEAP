8047ec399977b93def2979c2fb11f76fccd8d19d
Bk49 <ericngyongwei@gmail.com>
2023-05-26 13:26:33 +0800

    Modify Error404 page to include illustrations

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,13 +1,29 @@
 import { useRouteError } from "react-router-dom";
+import Container from "@mui/material/Container";
+import Error404Illustration from "../assets/illustrations/error404-illustration.jpg";
 
 const Error = () => {
     const error = useRouteError;
-    console.error(error);
+    console.log(error);
 
     return (
-        <>
-            <span>The page you have requested is not found</span>
-        </>
+        <Container
+            sx={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <img
+                style={{ height: "30vw", width: "30vw" }}
+                src={Error404Illustration}
+                alt="Error404 Illustration"
+            />
+            <span style={{fontFamily:"Cocogoose"}}>The page you have requested is not found</span>
+        </Container>
     );
 };
 
