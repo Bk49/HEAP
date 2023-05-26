1a09650f62cb8ee6a74b1eca6f6302c0fafc6480
Bk49 <ericngyongwei@gmail.com>
2023-05-26 13:57:23 +0800

    Modify Checkbox to be controlled using useFormContext hook

--- frontend/src/components/common/form/Checkbox.js
+++ frontend/src/components/common/form/Checkbox.js
@@ -1,19 +1,29 @@
 import FormControlLabel from "@mui/material/FormControlLabel";
 import MUICheckbox from "@mui/material/Checkbox";
+import { Controller, useFormContext } from "react-hook-form";
 
-const Checkbox = ({ label = "" }) => {
+const Checkbox = ({ name = "checkbox", label = "" }) => {
+    const { control } = useFormContext();
+
     return (
-        <FormControlLabel
-            control={
-                <MUICheckbox
-                    sx={{
-                        "&.Mui-checked": {
-                            color: "#163172",
-                        },
-                    }}
+        <Controller
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormControlLabel
+                    control={
+                        <MUICheckbox
+                            {...field}
+                            sx={{
+                                "&.Mui-checked": {
+                                    color: "#163172",
+                                },
+                            }}
+                        />
+                    }
+                    label={label}
                 />
-            }
-            label={label}
+            )}
         />
     );
 };
