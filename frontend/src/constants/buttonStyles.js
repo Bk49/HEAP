cd5dabc5c308beb3bb633b574aff5da4281a1253
Bk49 <ericngyongwei@gmail.com>
2023-05-31 15:57:56 +0800

    Add TextIconButton styles into constants

--- frontend/src/constants/buttonStyles.js
+++ frontend/src/constants/buttonStyles.js
@@ -9,3 +9,9 @@ export const bigButtonStyles = {
     primary: { backgroundColor: "#163172", hoverColor: "#0E1E43" },
     secondary: { backgroundColor: "#43ACD9", hoverColor: "#398DB0" },
 };
+
+export const textIconButtonStyles = {
+    primary: { backgroundColor: "#163172", hoverColor: "#0E1E43" },
+    secondary: { backgroundColor: "#43ACD9", hoverColor: "#398DB0" },
+    error: { backgroundColor: "#F44336", hoverColor: "#D32F2F" },
+};
