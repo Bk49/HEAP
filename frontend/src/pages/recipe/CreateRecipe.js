2212b7553fbc2b7fa068387b280181b954a2249c
Bk49 <ericngyongwei@gmail.com>
2023-05-31 16:52:49 +0800

    Modify CreateRecipe based on CommonRecipeForm

--- frontend/src/pages/recipe/CreateRecipe.js
+++ frontend/src/pages/recipe/CreateRecipe.js
@@ -1,122 +1,12 @@
-import { Fragment, useEffect, useState } from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import SubmitFormGroup from "../../components/common/form/SubmitFormGroup";
-import HeadingTwo from "../../components/common/heading/HeadingTwo";
-import TextField from "../../components/common/form/TextField";
-import SingleItemDropdown from "../../components/common/form/SingleItemDropdown";
-import { recipeCategories } from "../../constants/dropdown-choices";
-import AttachFileIcon from "@mui/icons-material/AttachFile";
+import { Fragment } from "react";
 import HeadingOne from "../../components/common/heading/HeadingOne";
-import FileInput from "../../components/common/form/FileInput";
-import FieldsColumn from "../../components/common/form/FieldsColumn";
-import FieldsRow from "../../components/common/form/FieldsRow";
-import TextArea from "../../components/common/form/TextArea";
-import ImagePlaceHolder from "../../assets/image-placeholder.png";
-import RecipeIngredientsFieldArray from "../../components/recipe/datarow/RecipeIngredientsFieldArray";
-import PreparationStepsFieldArray from "../../components/recipe/datarow/PreparationStepsFieldArray";
+import CommonRecipeForm from "../../components/recipe/CommonRecipeForm";
 
 const CreateRecipe = () => {
-    const formMethods = useForm();
-    const { watch } = formMethods;
-    const imageFile = watch("image");
-    const [imgUrl, setImgUrl] = useState(null);
-    const reader = new FileReader();
-
-    useEffect(() => {
-        reader.onload = () => {
-            setImgUrl(reader.result);
-        };
-
-        if (imageFile) {
-            reader.readAsDataURL(imageFile);
-        }
-    }, [imageFile]);
-
     return (
         <Fragment>
             <HeadingOne divider={true}>Create Recipe</HeadingOne>
-            <FormProvider {...formMethods}>
-                <HeadingTwo>Recipe Information</HeadingTwo>
-                <FieldsColumn>
-                    <FieldsRow>
-                        <TextField
-                            rules={{ required: true }}
-                            name="name"
-                            label="Recipe Name"
-                        />
-                        <SingleItemDropdown
-                            name="category"
-                            rules={{ required: true }}
-                            label="Category"
-                            choices={recipeCategories}
-                        />
-                        <TextField
-                            rules={{ required: true, min: 0 }}
-                            name="cost"
-                            label="Cost to Produce"
-                        />
-                        <FileInput
-                            rules={{ required: true }}
-                            name="image"
-                            label="Recipe Image"
-                            icon={<AttachFileIcon />}
-                        />
-                    </FieldsRow>
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            gap: "1rem",
-                            justifyContent: "space-between",
-                        }}
-                    >
-                        <FieldsColumn>
-                            <TextArea
-                                rule={{ required: true }}
-                                name="description"
-                                label="Description"
-                            />
-                        </FieldsColumn>
-                        <FieldsColumn>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    justifyContent: "space-between",
-                                    marginRight: "2.5rem",
-                                    alignItems: "center",
-                                    height: "100%",
-                                    width: "100%",
-                                }}
-                            >
-                                <img
-                                    style={{
-                                        height: "11rem",
-                                        width: "18rem",
-                                        objectFit: "cover",
-                                        borderRadius: "4px",
-                                    }}
-                                    alt="PreviewImage"
-                                    src={imgUrl ? imgUrl : ImagePlaceHolder}
-                                />
-                                <span>Recipe Image Preview</span>
-                            </div>
-                        </FieldsColumn>
-                    </div>
-                </FieldsColumn>
-
-                <HeadingTwo>Recipe Preparation</HeadingTwo>
-                <br />
-                <RecipeIngredientsFieldArray />
-                <br />
-                <PreparationStepsFieldArray />
-                <SubmitFormGroup
-                    submitErrorText="Creation of recipe is unsuccessful, please check your input"
-                    onSubmit={(data) => {
-                        console.log(data);
-                    }}
-                />
-            </FormProvider>
+            <CommonRecipeForm />
         </Fragment>
     );
 };
