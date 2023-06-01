e504bae5f83939b2156d409e41401b9378d9e9aa
Eugene Lian <eugene.lian.2022@smu.edu.sg>
2023-06-01 13:37:04 +0800

    pom.xml update

--- backend/src/main/java/com/heap/backend/BackendApplication.java
+++ backend/src/main/java/com/heap/backend/BackendApplication.java
@@ -2,9 +2,7 @@ package com.heap.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BackendApplication{
