b5aca81bef60f6a161cf3d0f8ca61940fd6df039
Bk49 <ericngyongwei@gmail.com>
2023-06-08 16:23:36 +0800

    Initial CreateMenu page

---
+++ frontend/src/pages/menu/CreateMenu.js
@@ -0,0 +1,14 @@
+import HeadingOne from "../../components/common/heading/HeadingOne";
+import { Fragment } from "react";
+import CommonMenuForm from "../../components/menu/CommonMenuForm";
+
+const CreateMenu = () => {
+    return (
+        <Fragment>
+            <HeadingOne divider={true}>Create Menu</HeadingOne>
+            <CommonMenuForm isCreate={true} />
+        </Fragment>
+    );
+};
+
+export default CreateMenu;
