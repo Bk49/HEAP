5966353c3951c007fe4f300d95dbee1b95ac3975
Bk49 <ericngyongwei@gmail.com>
2023-06-14 15:23:53 +0800

    Create AuthenticationController to map URLs to their responding authentication APIs

---
+++ backend/src/main/java/com/heap/backend/controller/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.heap.backend.controller;
+
+import com.heap.backend.data.request.AuthenticationRequest;
+import com.heap.backend.data.response.AuthenticationResponse;
+import com.heap.backend.service.auth.AuthenticationService;
+import com.heap.backend.data.request.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register (
+            @RequestBody RegisterRequest request
+    ){
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate (
+            @RequestBody AuthenticationRequest request
+    ){
+
+        return ResponseEntity.ok(service.authenticate(request));
+
+    }
+}
