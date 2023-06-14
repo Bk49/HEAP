a6b11c61668f9afb32eae261c65460955075f5be
Bk49 <ericngyongwei@gmail.com>
2023-06-14 15:10:23 +0800

    Refactor User class to implement Java Spring's UserDetails

--- backend/src/main/java/com/heap/backend/models/User.java
+++ backend/src/main/java/com/heap/backend/models/User.java
@@ -1,83 +1,73 @@
 package com.heap.backend.models;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Document(collection = "users")
-public class User {
-  @Id
-  private String id;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class User implements UserDetails {
+    @Id
+    private String id;
 
-  @NotBlank
-  @Size(max = 20)
-  private String username;
+    @NotBlank
+    @Email
+    private String email;
 
-  @NotBlank
-  @Size(max = 50)
-  @Email
-  private String email;
+    @NotBlank
+    @Size(max = 120)
+    private String password;
 
-  @NotBlank
-  @Size(max = 120)
-  private String password;
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 
-  @DBRef
-  private Set<Role> roles = new HashSet<>();
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(() -> "USER");
+        return roles;
+    }
 
-  public User() {
-  }
+    @Override
+    public String getUsername() {
+        return null;
+    }
 
-  public User(String username, String email, String password) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-  }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-  public String getId() {
-    return id;
-  }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-  public void setId(String id) {
-    this.id = id;
-  }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public Set<Role> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
-  }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
