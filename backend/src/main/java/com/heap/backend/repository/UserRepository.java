f069aa095f8626f8f5dd0eac7bd9c79d2a440c02
Bk49 <ericngyongwei@gmail.com>
2023-06-14 15:12:50 +0800

    Create UserRepository to find a user by using email

---
+++ backend/src/main/java/com/heap/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.heap.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.heap.backend.models.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+  Optional<User> findByEmail(String email);
+}
