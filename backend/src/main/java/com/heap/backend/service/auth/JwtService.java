253e02f300e8edf572e0d330c9a83ac5c346bcad
Eugene Lian <eugene.lian.2022@smu.edu.sg>
2023-06-19 15:06:53 +0800

    Secret key embedded in application.properties instead of jwtService.java

--- backend/src/main/java/com/heap/backend/service/auth/JwtService.java
+++ backend/src/main/java/com/heap/backend/service/auth/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,9 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+
+    @Value("${heap.backend.app.jwtSecretKey}")
+    private String SECRET_KEY;
 
     public String extractEmail(String token) {
         return extractClaim(token, Claims::getSubject);
