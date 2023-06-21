6438aa912ddadd283a113a358c61fe9318bafa69
Bk49 <ericngyongwei@gmail.com>
2023-06-21 17:36:56 +0800

    Initial EditBusinessGrowthPlan page for editing existing business growth plans

---
+++ frontend/src/pages/business/EditBusinessGrowthPlan.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import HeadingOne from "../../components/common/heading/HeadingOne";
+import CommonBusinessGrowthPlanForm from "../../components/business/CommonBusinessGrowthPlanForm";
+
+const EditBusinessGrowthPlan = () => {
+    return (
+        <Fragment>
+            <HeadingOne>Edit Business Growth Plan</HeadingOne>
+            <CommonBusinessGrowthPlanForm isCreate={false} />
+        </Fragment>
+    );
+};
+
+export default EditBusinessGrowthPlan
