0687f38dcf07e2c9ff0db962d3c9a393652e12d6
Bk49 <ericngyongwei@gmail.com>
2023-06-26 17:27:24 +0800

    Remove unnecessary imports in AuthenticationService.java

--- backend/src/main/java/com/heap/backend/service/auth/AuthenticationService.java
+++ backend/src/main/java/com/heap/backend/service/auth/AuthenticationService.java
@@ -9,8 +9,6 @@ import com.heap.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
