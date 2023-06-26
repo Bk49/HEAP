0e0341ab70b56b29ded4c4ec4ed678202df3a67e
Bk49 <ericngyongwei@gmail.com>
2023-06-26 17:24:12 +0800

    Create DemoController for testing

---
+++ backend/src/main/java/com/heap/backend/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.heap.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+@RequiredArgsConstructor
+public class DemoController {
+    @GetMapping("/")
+    public ResponseEntity<String> testing (){
+        return ResponseEntity.ok("Hello from secured endpoint");
+    }
+}
