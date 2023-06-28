2e7a05385c4a4c46e7ddebc60a29a41e92cc5882
Bk49 <ericngyongwei@gmail.com>
2023-06-28 10:52:27 +0800

    Initial temporary MySummary Page

---
+++ frontend/src/pages/business/MySummary.js
@@ -0,0 +1,12 @@
+import HeadingOne from "../../components/common/heading/HeadingOne";
+import { Fragment } from "react";
+
+const MySummary = () => {
+    return (
+        <Fragment>
+            <HeadingOne divider={true}>Summary</HeadingOne>
+        </Fragment>
+    );
+};
+
+export default MySummary;
