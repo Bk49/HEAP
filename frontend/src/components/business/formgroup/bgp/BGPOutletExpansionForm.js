54af594d77fcdeb99957de40def3bf0bc4fc3196
amelia-chew-2022 <amelia.chew.2022@scis.smu.edu.sg>
2023-07-05 12:00:05 +0800

    Started on BGP Outlet Expansion Form

--- frontend/src/components/business/formgroup/bgp/BGPOutletExpansionForm.js
+++ frontend/src/components/business/formgroup/bgp/BGPOutletExpansionForm.js
@@ -1,7 +1,44 @@
 import { Fragment } from "react";
+import HeadingThree from "../../../common/heading/HeadingThree";
+import FieldsColumn from "../../../common/form/FieldsColumn";
+import FieldsRow from "../../../common/form/FieldsRow";
+import TextField from "../../../common/form/TextField";
+import SingleItemDropdown from "../../../common/form/SingleItemDropdown";
 
 const BGPOutletExpansionForm = () => {
-    return <Fragment></Fragment>;
+    return {
+        <Fragment>
+            <HeadingThree>Outlet Expansion Address and Location</HeadingThree>
+            <FieldsColumn>
+            <FieldsRow>
+                <TextField
+                    rules={{ required: true }}
+                    label="Outlet Expansion Address and Location"
+                    name="address"
+                />
+            </FieldsRow>
+            </FieldsColumn>
+
+            <HeadingThree>Staffs</HeadingThree>
+            <FieldsRow>
+                <TextField
+                    rules={{ required: true }}
+                    label="Staff Name"
+                    name="staffName"
+                />
+                 <SingleItemDropdown
+                    rules={{ required: true }}
+                    label="Hiring Method"
+                    name="hiringMethod"
+                />
+                <SingleItemDropdown
+                    rules={{ required: true }}
+                    label="Position"
+                    name="position"
+                />
+            </FieldsRow>
+        </Fragment>
+    };
 };
 
 export default BGPOutletExpansionForm;
