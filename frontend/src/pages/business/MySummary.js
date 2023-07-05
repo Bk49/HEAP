7400a8df2dcc20ca9578faf014c11947ea1b5f0f
Bk49 <ericngyongwei@gmail.com>
2023-07-05 09:58:59 +0800

    Create temporary button under MySummary page to navigate to Create Plan page

--- frontend/src/pages/business/MySummary.js
+++ frontend/src/pages/business/MySummary.js
@@ -1,10 +1,15 @@
+import { Button } from "@mui/material";
 import HeadingOne from "../../components/common/heading/HeadingOne";
 import { Fragment } from "react";
+import {useNavigate} from "react-router-dom"
 
 const MySummary = () => {
+    const navigate = useNavigate()
+
     return (
         <Fragment>
             <HeadingOne divider={true}>Summary</HeadingOne>
+            <Button onClick={() => navigate("/create-plan")}>Create</Button>
         </Fragment>
     );
 };
