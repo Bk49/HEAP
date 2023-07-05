79d3e536f9b4f4208f19f2574a3f054ede06e683
Bk49 <ericngyongwei@gmail.com>
2023-07-05 09:57:39 +0800

    Initial PosterNBannerMkForm for rendering Poster and Banner Marketing specific form components

---
+++ frontend/src/components/business/formgroup/marketing/PosterNBannerMkForm.js
@@ -0,0 +1,39 @@
+import { Fragment } from "react";
+import HeadingThree from "../../../common/heading/HeadingThree";
+import FieldsColumn from "../../../common/form/FieldsColumn";
+import FieldsRow from "../../../common/form/FieldsRow";
+import FileInput from "../../../common/form/FileInput";
+import TextField from "../../../common/form/TextField";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+
+const PosterNBannerMkForm = () => {
+    return (
+        <Fragment>
+            <HeadingThree>Other Posts and Banner Details</HeadingThree>
+            <FieldsColumn>
+                <FieldsRow>
+                    <FileInput
+                        icon={<AttachFileIcon />}
+                        rules={{ required: true }}
+                        label="Poster Design (File)"
+                        name="posterDesign"
+                    />
+                    <TextField
+                        type="number"
+                        rules={{ required: true, min: 0 }}
+                        label="Cost/piece"
+                        name="posterCost"
+                    />
+                    <TextField
+                        type="number"
+                        rules={{ required: true, min: 0 }}
+                        label="Quantity"
+                        name="posterQuantity"
+                    />
+                </FieldsRow>
+            </FieldsColumn>
+        </Fragment>
+    );
+};
+
+export default PosterNBannerMkForm;
