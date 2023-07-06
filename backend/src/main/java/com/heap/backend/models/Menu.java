9e8ca3929190b60f92a42f69164d184960ad3a0c
Eugene Lian <eugene.lian.2022@smu.edu.sg>
2023-07-06 14:37:59 +0800

    Removed Equals method in MenuService

--- backend/src/main/java/com/heap/backend/models/Menu.java
+++ backend/src/main/java/com/heap/backend/models/Menu.java
@@ -31,22 +31,4 @@ public class Menu {
     @NotBlank
     private String type;
 
-    public boolean equals(Menu menu) {
-
-        boolean equal = this.name.equals(menu.name) && type.equals(menu.name) && this.sections.length == menu.sections.length;
-
-        if (!equal) {
-            return false;
-        }
-
-        for (int i = 0 ; i < menu.sections.length ; i++) {
-
-            if (this.sections[i].equals(menu.sections[i])) {
-                return false;
-            }
-
-        }
-
-        return true;
-    }
 }
--- backend/src/main/java/com/heap/backend/models/MenuSection.java
+++ backend/src/main/java/com/heap/backend/models/MenuSection.java
@@ -13,22 +13,4 @@ public class MenuSection {
     private String name;
     private Item[] items;
 
-    public boolean equals(MenuSection ms) {
-
-        boolean equal = this.name.equals(ms.name) && this.items.length == ms.items.length;
-
-        if (!equal) {
-            return false;
-        }
-
-        for (int i = 0 ; i < items.length ; i++) {
-
-            if (this.items[i].equals(ms.items[i])) {
-                return false;
-            }
-
-        }
-
-        return true;
-    }
 }
