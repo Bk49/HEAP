4145f87776857fff46c1a31ebae1b00126337488
Bk49 <ericngyongwei@gmail.com>
2023-07-10 12:20:30 +0800

    Refactor Error.js to render the correct AppBar

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,29 +1,46 @@
-import { useRouteError } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Error404Illustration from "../assets/illustrations/error404-illustration.jpg";
+import jwtDecode from "jwt-decode";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import BaseRoute from "../router/routes/BaseRoute";
+import NavBar from "../components/common/navigations/NavBar";
 
 const Error = () => {
-    const error = useRouteError;
-    console.log(error);
+    const [authenticated, setAuthenticated] = useState(false);
+
+    useEffect(() => {
+        try {
+            jwtDecode(Cookies.get("token"));
+            setAuthenticated(true);
+        } catch (e) {
+            setAuthenticated(false);
+        }
+    }, []);
 
     return (
-        <Container
-            sx={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <img
-                style={{ height: "30vw", width: "30vw" }}
-                src={Error404Illustration}
-                alt="Error404 Illustration"
-            />
-            <span style={{fontFamily:"Cocogoose"}}>The page you have requested is not found</span>
-        </Container>
+        <BaseRoute>
+            <NavBar authenticated={authenticated} />
+            <Container
+                sx={{
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <img
+                    style={{ height: "30vw", width: "30vw" }}
+                    src={Error404Illustration}
+                    alt="Error404 Illustration"
+                />
+                <span style={{ fontFamily: "Cocogoose" }}>
+                    The page you have requested is not found
+                </span>
+            </Container>
+        </BaseRoute>
     );
 };
 
