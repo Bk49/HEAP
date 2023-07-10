aeb4dd7776fd9e6024a4e3c9b7bd4e4212440881
amelia-chew-2022 <amelia.chew.2022@scis.smu.edu.sg>
2023-07-10 20:57:59 +0800

    Added GoogleMaps API and maps folder in components

---
+++ frontend/src/components/business/map/Map.js
@@ -0,0 +1,11 @@
+// import { Map } from "@googlemaps/react-wrapper";
+
+// function Map() {
+//     return (
+//         <div>
+//             <Map apiKey="YOUR_API_KEY" />
+//         </div>
+//     );
+// }
+
+// export default Map;
