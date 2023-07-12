634e083ffb867042f6747ea90bc4e0c9fc250921
Bk49 <ericngyongwei@gmail.com>
2023-07-12 09:36:37 +0800

    Swapping of token usage for deleteRecipeAPI

--- frontend/src/axios/recipe/deleteRecipeAPI.js
+++ frontend/src/axios/recipe/deleteRecipeAPI.js
@@ -1,17 +1,17 @@
+import Cookies from "js-cookie";
 import { protectedInstance as instance } from "../instance";
 
 const deleteRecipe = async (id) => {
     try {
         const result = await instance.delete(`/user/deleteRecipe/${id}`, {
-            headers: { 
-                // Authorization: `Bearer ${Cookies.get("token")}`
-                Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3ZXNseS5jaGF1LjIwMjJAc2Npcy5zbXUuZWR1LnNnIiwiaWF0IjoxNjg5MDU1NzM0LCJleHAiOjE2ODkxNDIxMzR9.ClN--RPNaPaUdonEGwRUsLm1dx1Xw-1atn5c4b1cjqw"}` 
+            headers: {
+                Authorization: `Bearer ${Cookies.get("token")}`,
+                // Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3ZXNseS5jaGF1LjIwMjJAc2Npcy5zbXUuZWR1LnNnIiwiaWF0IjoxNjg5MDU1NzM0LCJleHAiOjE2ODkxNDIxMzR9.ClN--RPNaPaUdonEGwRUsLm1dx1Xw-1atn5c4b1cjqw"}`,
             },
         });
         return result.data.response;
     } catch (e) {
         let msg = "";
-        console.log(e);
         if (!e.response) {
             msg =
                 "Delete recipe unsuccessful due to network error!\nPlease check your internet connection!";
@@ -25,4 +25,3 @@ const deleteRecipe = async (id) => {
 };
 
 export default deleteRecipe;
-
