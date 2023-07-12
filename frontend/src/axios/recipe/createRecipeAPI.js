487a2d819ed9743c4f97ffbbb7555a2042f5be73
Bk49 <ericngyongwei@gmail.com>
2023-07-12 09:36:06 +0800

    Complete integration of image uploading with createRecipeAPI

--- frontend/src/axios/recipe/createRecipeAPI.js
+++ frontend/src/axios/recipe/createRecipeAPI.js
@@ -1,16 +1,28 @@
 import Cookies from "js-cookie";
 import { protectedInstance as instance } from "../instance";
+import handleImageUpload from "../../functions/uploadImage";
 
 const createRecipe = async (data) => {
-    const processData = {...data, image: ""}
     try {
-        const result = await instance.post(`/user/createRecipe`, processData, {
-            headers: { Authorization: `Bearer ${Cookies.get("token")}` },
-        });
-        return result.data;
+        const { image, ...other } = data;
+        const { Location } = await handleImageUpload(
+            image,
+            `recipe/${other.name}/recipe_image${Date.now()}`
+        );
+
+        const result = await instance.post(
+            `/user/createRecipe`,
+            { ...other, image: Location },
+            {
+                headers: { Authorization: `Bearer ${Cookies.get("token")}` },
+                // headers: {
+                //     Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlcmljbmd5b25nd2VpQGdtYWlsLmNvbSIsImlhdCI6MTY4OTA0MDQyMSwiZXhwIjoxNjg5MTI2ODIxfQ.dq4Xgx8AColJm86n4vJPOvzhVQs221XxoaHvbW74q1w"}`,
+                // },
+            }
+        );
+        return result.data.response;
     } catch (e) {
         let msg = "";
-        console.log(e);
         if (!e.response) {
             msg =
                 "Create recipe unsuccessful due to network error!\nPlease check your internet connection!";
