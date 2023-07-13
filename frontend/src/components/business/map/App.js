be69bb76a76e763d601e493413fae88d79d5c2e1
amelia-chew-2022 <amelia.chew.2022@scis.smu.edu.sg>
2023-07-13 22:05:42 +0800

    Change the spacings in App.js

--- frontend/src/components/business/map/App.js
+++ frontend/src/components/business/map/App.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
-    width: '652px',
-    height: '342px',
+    width: "652px",
+    height: "342px",
 };
 
 const center = {
-  lat: 1.3521,
-  lng: 103.8198
+    lat: 1.3521,
+    lng: 103.8198,
 };
 
 const Map = () => {
-  return (
-    <LoadScript googleMapsApiKey="AIzaSyChsCzm5-iAjK2cMpj_garxpAQdC4YbqsE">
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      >
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
-  );
+    return (
+        <LoadScript googleMapsApiKey="AIzaSyChsCzm5-iAjK2cMpj_garxpAQdC4YbqsE">
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+            >
+                <Marker position={center} />
+            </GoogleMap>
+        </LoadScript>
+    );
 };
 
 function App() {
-  return (
-    <div>
-      <Map />
-    </div>
-  );
+    return (
+        <div>
+            <Map />
+        </div>
+    );
 }
 
 export default App;
