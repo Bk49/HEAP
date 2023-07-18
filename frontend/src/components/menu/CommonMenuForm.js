a49330acda4ff32235ff9509d0a2f1534e2e6098
unknown <wesly.chau.li.zhan.01@gmail.com>
2023-07-19 01:22:39 +0800

    editted CommonMenuForm

--- frontend/src/components/menu/CommonMenuForm.js
+++ frontend/src/components/menu/CommonMenuForm.js
@@ -12,6 +12,7 @@ import { queueError } from "../../functions/formHandling";
 import { enqueueSnackbar } from "notistack";
 import createMenu from "../../axios/menu/createMenuAPI";
 import { useNavigate } from "react-router-dom";
+import updateMenu from "../../axios/menu/updateMenuAPI";
 
 const CommonMenuForm = ({ isCreate = false, loaderData}) => {
     const formMethods = useForm({
