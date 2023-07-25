293a95114cd49dcf1228ac8c757e72b07a9440f2
Bk49 <ericngyongwei@gmail.com>
2023-07-25 09:47:25 +0800

    Add image field and preview for menu form

--- frontend/src/components/menu/CommonMenuForm.js
+++ frontend/src/components/menu/CommonMenuForm.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import FieldsColumn from "../common/form/FieldsColumn";
 import HeadingTwo from "../common/heading/HeadingTwo";
 import FieldsRow from "../common/form/FieldsRow";
@@ -13,32 +13,90 @@ import { enqueueSnackbar } from "notistack";
 import createMenu from "../../axios/menu/createMenuAPI";
 import { useNavigate } from "react-router-dom";
 import updateMenu from "../../axios/menu/updateMenuAPI";
+import FileInput from "../common/form/FileInput";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import ImagePlaceHolder from "../../assets/image-placeholder.png";
+import Grid from "@mui/material/Grid";
 
-const CommonMenuForm = ({ isCreate = false, loaderData}) => {
+const CommonMenuForm = ({ isCreate = false, loaderData }) => {
     const formMethods = useForm({
         values: !isCreate && loaderData ? loaderData : {},
     });
+    const { watch } = formMethods;
+    const imageFile = watch("image");
     const navigate = useNavigate();
+    const [imgUrl, setImgUrl] = useState(null);
+
+    useEffect(() => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            setImgUrl(reader.result);
+        };
+
+        if (imageFile) {
+            /^https?:\/\/.+/i.test(imageFile)
+                ? setImgUrl(imageFile)
+                : reader.readAsDataURL(imageFile);
+        }
+    }, [imageFile]);
 
     return (
         <Fragment>
             <FormProvider {...formMethods}>
                 <HeadingTwo>Menu Information</HeadingTwo>
-                <FieldsColumn>
-                    <FieldsRow>
-                        <TextField
-                            rules={{ required: true }}
-                            name="name"
-                            label="Menu Name"
-                        />
-                        <SingleItemDropdown
-                            rules={{ required: true }}
-                            name="type"
-                            label="Menu Type"
-                            choices={menuTypes}
-                        />
-                    </FieldsRow>
-                </FieldsColumn>
+                <Grid container>
+                    <Grid xs={9}>
+                        <FieldsColumn>
+                            <FieldsRow>
+                                <TextField
+                                    rules={{ required: true }}
+                                    name="name"
+                                    label="Menu Name"
+                                />
+                                <SingleItemDropdown
+                                    rules={{ required: true }}
+                                    name="type"
+                                    label="Menu Type"
+                                    choices={menuTypes}
+                                />
+                            </FieldsRow>
+                            <FieldsRow>
+                                <FileInput
+                                    rules={{ required: false }}
+                                    name="image"
+                                    label="Menu Image"
+                                    icon={<AttachFileIcon />}
+                                />
+                            </FieldsRow>
+                        </FieldsColumn>
+                    </Grid>
+                    <Grid xs={3}>
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "space-between",
+                                marginRight: "2.5rem",
+                                alignItems: "center",
+                                height: "100%",
+                                width: "100%",
+                            }}
+                        >
+                            <img
+                                style={{
+                                    height: "11rem",
+                                    width: "11rem",
+                                    objectFit: "cover",
+                                    borderRadius: "4px",
+                                }}
+                                alt="PreviewImage"
+                                src={imgUrl ? imgUrl : ImagePlaceHolder}
+                            />
+                            <span>Menu Image Preview</span>
+                        </div>
+                    </Grid>
+                </Grid>
+
                 <HeadingTwo>Menu Details</HeadingTwo>
                 <MenuSectionFieldsArray />
                 <SubmitFormGroup
@@ -47,12 +105,13 @@ const CommonMenuForm = ({ isCreate = false, loaderData}) => {
                     } of menu is unsuccessful, please check your input`}
                     onSubmit={async (data) => {
                         try {
+                            console.log(data)
                             const res = isCreate
                                 ? await createMenu(data)
                                 : await updateMenu(data, data.id);
                             navigate("/my-menus", {
                                 state: { success: res },
-                            }); 
+                            });
                         } catch (e) {
                             queueError(e, enqueueSnackbar);
                         }
