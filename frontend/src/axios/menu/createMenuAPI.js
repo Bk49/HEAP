2bc2235f5961c156f31265c3f9fc42ca28be599b
Bk49 <ericngyongwei@gmail.com>
2023-07-25 09:47:49 +0800

    Integrate image and file uploading for menu apis

--- frontend/src/axios/menu/createMenuAPI.js
+++ frontend/src/axios/menu/createMenuAPI.js
@@ -1,14 +1,20 @@
 import Cookies from "js-cookie";
 import { protectedInstance as instance } from "../instance";
+import handleImageUpload from "../../functions/uploadImage";
 
 const createMenu = async (data) => {
     try {
+        const { image, ...other } = data;
+        const imgUrl = await handleImageUpload(
+            image,
+            `menu/${other.name}/menu_image${Date.now()}`
+        );
 
         const result = await instance.post(
             `/user/createMenu`,
-            data,
+            { ...other, image: imgUrl },
             {
-                headers: { Authorization: `Bearer ${Cookies.get("token")}` },                
+                headers: { Authorization: `Bearer ${Cookies.get("token")}` },
                 // headers: {
                 //     Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3ZXNseS5jaGF1LjIwMjJAc2Npcy5zbXUuZWR1LnNnIiwiaWF0IjoxNjg5Njk4MDkzLCJleHAiOjE2ODk3ODQ0OTN9.u0fM6KsZQ9tJbYSe1BWw3mgRndBIBBORM_TLrzviGb4"}`,
                 // },
