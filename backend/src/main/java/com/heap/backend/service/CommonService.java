dc7fee745d5a25eb7f52169e59272dcab53e230c
Bk49 <ericngyongwei@gmail.com>
2023-07-25 14:32:01 +0800

    Fix for refactoring breaking issues

--- backend/src/main/java/com/heap/backend/service/CommonService.java
+++ backend/src/main/java/com/heap/backend/service/CommonService.java
@@ -1,23 +1,23 @@
-package com.heap.backend.service;
-
-import com.heap.backend.data.response.Response;
-import com.heap.backend.data.response.common.ErrorResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class CommonService {
-    @Bean
-    public ResponseEntity<Response> checkResponse(Response response) {
-        if (response instanceof ErrorResponse errorResponse) {
-            return errorResponse.getMessage().contains("Bad Request")
-                    ? ResponseEntity.badRequest().body(errorResponse)
-                    : ResponseEntity.internalServerError().body(errorResponse);
-        }
-
-        return ResponseEntity.ok(response);
-    }
-}
+//package com.heap.backend.service;
+//
+//import com.heap.backend.data.response.Response;
+//import com.heap.backend.data.response.common.ErrorResponse;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class CommonService {
+//    @Bean
+//    public ResponseEntity<Response> checkResponse(Response response) {
+//        if (response instanceof ErrorResponse errorResponse) {
+//            return errorResponse.getMessage().contains("Bad Request")
+//                    ? ResponseEntity.badRequest().body(errorResponse)
+//                    : ResponseEntity.internalServerError().body(errorResponse);
+//        }
+//
+//        return ResponseEntity.ok(response);
+//    }
+//}
