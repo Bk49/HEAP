2e8a3710698a03411ab2f8fda156ca8711a1ac2c
Bk49 <ericngyongwei@gmail.com>
2023-07-25 15:31:09 +0800

    Create CommonService class to handle checkResponse and returnOldEmail

--- backend/src/main/java/com/heap/backend/service/CommonService.java
+++ backend/src/main/java/com/heap/backend/service/CommonService.java
@@ -1,23 +1,31 @@
-//package com.heap.backend.service;
-//
-//import com.heap.backend.data.response.Response;
-//import com.heap.backend.data.response.common.ErrorResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CommonService {
-//    @Bean
-//    public ResponseEntity<Response> checkResponse(Response response) {
-//        if (response instanceof ErrorResponse errorResponse) {
-//            return errorResponse.getMessage().contains("Bad Request")
-//                    ? ResponseEntity.badRequest().body(errorResponse)
-//                    : ResponseEntity.internalServerError().body(errorResponse);
-//        }
-//
-//        return ResponseEntity.ok(response);
-//    }
-//}
+package com.heap.backend.service;
+
+import com.heap.backend.data.response.Response;
+import com.heap.backend.data.response.common.ErrorResponse;
+import com.heap.backend.service.auth.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommonService {
+    //    This defaultResponse is on purpose so that it @Autowires DefaultResponse class instead of Response implementation
+    private final Response defaultResponse;
+    private final JwtService jwtService;
+
+    public ResponseEntity<Response> checkResponse(Response response) {
+        if (response instanceof ErrorResponse errorResponse) {
+            return errorResponse.getMessage().contains("Bad Request")
+                    ? ResponseEntity.badRequest().body(errorResponse)
+                    : ResponseEntity.internalServerError().body(errorResponse);
+        }
+
+        return ResponseEntity.ok(response);
+    }
+
+    public String returnOldEmail(String token) {
+        String jwt = token.substring(7);
+        return jwtService.extractEmail(jwt);
+    }
+}
