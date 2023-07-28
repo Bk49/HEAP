a69e4c5768853287864683120da7ed0453a36279
Damien Tan <tyxdamien@Damiens-MacBook-Air.local>
2023-07-28 23:29:34 +0800

    Add SortingButton

--- frontend/src/components/common/button/SortingButton.js
+++ frontend/src/components/common/button/SortingButton.js
@@ -1,31 +1,82 @@
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import { smallButtonStyles as style } from "../../../constants/buttonStyles";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const SortingButton = ({
+    sortingOrder,
     children = "Button Text",
-    type = "default",
     onClick = () => {
         console.log("Button pressed");
     },
 }) => {
+   
+    const [sortState, setSortState] = useState('default');
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleSorting = () => {
+      if (sortState === 'default') {
+        setSortState('asc');
+      } else if (sortState === 'asc') {
+        setSortState('desc');
+      } else {
+        setSortState('default');
+      }
+        setIsClicked(true);
+        onClick();
+    };
+  
+    const getButtonContent = () => {
+      if (sortState === 'default') {
+        return;
+      } else if (sortState === 'asc') {
+        return (
+            <ExpandMoreIcon />
+        );
+      } else {
+        return (
+             <ExpandLessIcon />
+        );
+      }
+    };
+  
+    const getButtonColor = () => {
+      if (sortState === 'default') {
+        return style["secondary"];
+      } else {
+        return style["primary"];
+      }
+    };
+
+    const getHoverColor = () => {
+        if (sortState === 'default') {
+          return style["secondary"].hoverColor;
+        } else {
+          return style["primary"].hoverColor;
+        }
+      };
+
     return (
         <Button
             sx={{
-                backgroundColor: style[type].backgroundColor,
+                backgroundColor: getButtonColor(),
                 color: "white",
                 boxShadow: "2",
                 width: "7rem",
                 "&:hover": {
-                    backgroundColor: style[type].hoverColor,
+                    backgroundColor: getHoverColor(),
                 },
             }}
-            variant="contain"
-            onClick={onClick}
+            variant="contained"
+            onClick={handleSorting}
         >
-            {children}
+            {children} {getButtonContent()}
         </Button>
     );
 };
 
 export default SortingButton;
+
+
 
