2a1ada80f357c50c17b154bd1e23946e0f999212
Damien Tan <tyxdamien@Damiens-MacBook-Air.local>
2023-07-28 23:31:01 +0800

    Update MyBGP

--- frontend/src/pages/business/MyBusinessGrowthPlan.js
+++ frontend/src/pages/business/MyBusinessGrowthPlan.js
@@ -1,161 +1,137 @@
+import React, { useState } from 'react';
 import { Grid, Paper } from "@mui/material";
 import HeadingOne from "../../components/common/heading/HeadingOne";
 import SortingButton from "../../components/common/button/SortingButton";
 import Divider from "@mui/material/Divider";
-import FoodDeliveryIllustration from "../../assets/illustrations/business/food-delivery-illustration.jpg";
-import AvTimerIcon from "@mui/icons-material/AvTimer";
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
-import PriorityHighIcon from "@mui/icons-material/PriorityHigh";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
 import { useNavigate } from "react-router-dom";
-import IconButton from "@mui/material/IconButton";
+import TextIconButton from "../../components/common/button/TextIconButton";
+import BusinessGrowthPlanCard from "../../components/business/card/BusinessGrowthPlanCard";
 
 const MyBusinessGrowthPlan = () => {
     const navigate = useNavigate();
+    const [sortingOrder, setSortingOrder] = useState("asc"); // Initialize to "asc"
+    const [cards, setCards] = useState([
+        {
+            id: "64bdef6b6b5d992c941e82ca",
+            planName: "MK 2",
+            startDate: "25/07/2023",
+            endDate: "27/07/2023",
+            budget: 20000,
+            priority: 5,
+            planType: "MK",
+        },
+        {
+            id: "64bdefaa6b5d992c941e82cb",
+            planName: "Flyer Dist 1",
+            startDate: "24/07/2023",
+            endDate: "25/07/2023",
+            budget: 10020,
+            priority: 1,
+            planType: "FD",
+        },
 
+        {
+            id: "64bdefaa6b5d992c941e82cc",
+            planName: "Flyer Dist 2",
+            startDate: "01/12/2023",
+            endDate: "16/12/2023",
+            budget: 3003.43,
+            priority: 3,
+            planType: "OE",
+        },
+        {
+            id: "64bdefaa6b5d992c941e82cd",
+            planName: "Flyer Dist 1",
+            startDate: "24/07/2023",
+            endDate: "25/07/2023",
+            budget: 10020,
+            priority: 1,
+            planType: "FD",
+        },
+    ]);
+  
+    const handleDelete = (id) => {
+      const updatedCards = cards.filter((card) => card.id !== id);
+      setCards(updatedCards);
+    };
+  
+    const sortCards = (buttons, sortOrder) => {
+      if (sortOrder === "asc") {
+
+        return buttons.sort((a, b) => a.priority - b.priority);
+      } else if (sortOrder === "desc") {
+
+        return buttons.sort((a, b) => b.priority - a.priority);
+      }
+
+      return buttons;
+    };
+  
+    const handleSort = () => {
+      if (sortingOrder === "asc") {
+        setSortingOrder("desc");
+      } else if (sortingOrder === "desc") {
+        setSortingOrder(null); 
+      } else {
+        setSortingOrder("asc"); 
+      }
+     
+      const sortedCards = sortCards([...cards], sortingOrder);
+      setCards(sortedCards);
+    };
+  
     return (
-        <div>
-            <HeadingOne> My Business Growth Plans </HeadingOne>
-            <div style={{ marginTop: "1rem" }}></div>
-            <Divider />
-            <Grid
-                sx={{
-                    width: "100%",
-                }}
-                container
-                flexDirection="row-reverse"
-                alignItems="end"
-                padding="1rem"
-                gap="1rem"
-                justifyContent="flex-start"
-            >
-                <SortingButton
-                    type="primary"
-                    onClick={() => navigate("/register")}
-                >
-                    + CREATE{" "}
-                </SortingButton>
-                <SortingButton type="secondary"> PRIORITY </SortingButton>
-                <SortingButton type="secondary"> URGENCY </SortingButton>
-            </Grid>
+      <div>
+        <HeadingOne> My Business Growth Plans </HeadingOne>
+        <div style={{ marginTop: "1rem" }}></div>
+        <Divider />
+        <Grid
+          sx={{
+            width: "100%",
+          }}
+          container
+          flexDirection="row-reverse"
+          alignItems="end"
+          padding="1rem"
+          gap="1rem"
+          justifyContent="flex-start"
+        >
+          <TextIconButton
+            type="primary"
+            onClick={() => navigate("/create-plan")}
+          >
+            Create
+          </TextIconButton>
+          <SortingButton
+            sortingOrder={sortingOrder}
+            onClick={handleSort}
 
-            <Grid
-                sx={{
-                    width: "100%",
-                }}
-                container
-            >
-                <Paper
-                    elevation={4}
-                    sx={{
-                        margin: "1rem 2rem 2rem",
-                        width: "100%",
-                        height: "12rem",
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "left",
-                        gap: "1rem",
-                        justifyContent: "left",
-                    }}
-                >
-                    <Grid
-                        item
-                        xs={11}
-                        container
-                        direction="row"
-                        alignItems="top"
-                    >
-                        <img
-                            src={FoodDeliveryIllustration}
-                            alt="FoodDeliveryIllustration"
-                            style={{ height: "90%" }}
-                        />
-                        <div style={{ marginTop: "2rem" }}>
-                            <span
-                                style={{
-                                    fontFamily: "Cocogoose",
-                                    fontSize: "13px",
-                                }}
-                            >
-                                GrabFood MktPlace
-                            </span>
-                            <div
-                                style={{
-                                    fontSize: "13px",
-                                    opacity: "0.6",
-                                    display: "flex",
-                                    justifyContent: "left",
-                                    alignItems: "center",
-                                    marginTop: "0.5rem",
-                                }}
-                            >
-                                <AvTimerIcon />{" "}
-                                <span style={{ marginLeft: "0.5rem" }}>
-                                    {" "}
-                                    1 May 2023 - 2 June 2023{" "}
-                                </span>
-                            </div>
-                            <div
-                                style={{
-                                    fontSize: "13px",
-                                    opacity: "0.6",
-                                    display: "flex",
-                                    justifyContent: "left",
-                                    alignItems: "center",
-                                }}
-                            >
-                                <AttachMoneyIcon />{" "}
-                                <span style={{ marginLeft: "0.5rem" }}>
-                                    20,000.00
-                                </span>
-                            </div>
-                            <div
-                                style={{
-                                    fontSize: "13px",
-                                    opacity: "0.6",
-                                    display: "flex",
-                                    justifyContent: "left",
-                                    alignItems: "center",
-                                }}
-                            >
-                                <PriorityHighIcon />{" "}
-                                <span style={{ marginLeft: "0.5rem" }}>
-                                    Very High Priority
-                                </span>
-                            </div>
-                        </div>
-                    </Grid>
-
-                    <Grid
-                        item
-                        xs={1}
-                        container
-                        direction="column"
-                        justifyContent="space-between"
-                        alignItems="end"
-                    >
-                        <Grid item>
-                            <div style={{ margin: "1rem", opacity: "0.6" }}>
-                                <IconButton
-                                    onClick={() => navigate("/create-plan")}
-                                >
-                                    <EditIcon />
-                                </IconButton>
-                            </div>
-                        </Grid>
-                        <Grid item>
-                            <div style={{ margin: "1rem" }}>
-                                <IconButton onClick={() => {}}>
-                                    <DeleteIcon />
-                                </IconButton>
-                            </div>
-                        </Grid>
-                    </Grid>
-                </Paper>
+          >
+            {" "}
+            PRIORITY{" "}
+          </SortingButton>
+          <SortingButton
+            sortingOrder={sortingOrder}
+            onClick={handleSort}
+          >
+            {" "}
+            URGENCY{" "}
+          </SortingButton>
+        </Grid>
+  
+        <Grid container direction="column">
+          {cards.map((card, index) => (
+            <Grid key={index} item>
+              <BusinessGrowthPlanCard
+                card={card}
+                onDelete={() => handleDelete(card.id)}
+              />
             </Grid>
-        </div>
+          ))}
+        </Grid>
+      </div>
     );
-};
+  };
+ 
+  export default MyBusinessGrowthPlan;
 
-export default MyBusinessGrowthPlan;
