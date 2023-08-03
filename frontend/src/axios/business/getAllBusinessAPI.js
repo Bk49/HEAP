49d095e5ace501b97098dbf34fe71f645d065c75
Bk49 <ericngyongwei@gmail.com>
2023-08-04 00:12:04 +0800

    Incorporate sorting for getAllBusinessAPI

--- frontend/src/axios/business/getAllBusinessAPI.js
+++ frontend/src/axios/business/getAllBusinessAPI.js
@@ -1,14 +1,17 @@
 import Cookies from "js-cookie";
 import { protectedInstance as instance } from "../instance";
 
-const getAllBusiness = async () => {
+const getAllBusiness = async ({ sortBy, direction = "descending" }) => {
     try {
-        const result = await instance.get(`/user/findAllBGP`, {
-            headers: { Authorization: `Bearer ${Cookies.get("token")}` },
-            // headers: {
-            //     Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlcmljbmd5b25nd2VpQGdtYWlsLmNvbSIsImlhdCI6MTY4OTA0MDQyMSwiZXhwIjoxNjg5MTI2ODIxfQ.dq4Xgx8AColJm86n4vJPOvzhVQs221XxoaHvbW74q1w"}`,
-            // },
-        });
+        const result = await instance.get(
+            `/user/findAllBGP?sortBy=${sortBy}&direction=${direction}`,
+            {
+                headers: { Authorization: `Bearer ${Cookies.get("token")}` },
+                // headers: {
+                //     Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlcmljbmd5b25nd2VpQGdtYWlsLmNvbSIsImlhdCI6MTY4OTA0MDQyMSwiZXhwIjoxNjg5MTI2ODIxfQ.dq4Xgx8AColJm86n4vJPOvzhVQs221XxoaHvbW74q1w"}`,
+                // },
+            }
+        );
 
         return result.data;
     } catch (e) {
