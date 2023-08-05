829e4050e60135d0e66d99d67833e2d3cfb47b42
Bk49 <ericngyongwei@gmail.com>
2023-08-05 16:59:24 +0800

    Uncomment front end route protection code

--- frontend/src/router/routes/PublicRoute.js
+++ frontend/src/router/routes/PublicRoute.js
@@ -2,28 +2,28 @@ import NavBar from "../../components/common/navigations/NavBar";
 import BaseRoute from "./BaseRoute";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { queueSuccess } from "../../functions/formHandling";
-import { enqueueSnackbar } from "notistack";
-import jwtDecode from "jwt-decode";
-import Cookies from "js-cookie";
+// import { queueSuccess } from "../../functions/formHandling";
+// import { enqueueSnackbar } from "notistack";
+// import jwtDecode from "jwt-decode";
+// import Cookies from "js-cookie";
 
 const PublicRoute = ({ children }) => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const token = Cookies.get("token");
+        // const token = Cookies.get("token");
 
-        if (token) {
-            try {
-                const {sub, exp} = jwtDecode(token)
-                if(sub.length > 0 && new Date(exp * 1000) > new Date()){
-                    return navigate("/my-summary", {state: {info: "Session found, redirect to Summary Page"}})
-                }
-            } catch (e) {
-                queueSuccess("Invalid/ Expired token found, removing token", enqueueSnackbar)
-                Cookies.set("token", null);
-            }
-        }
+        // if (token) {
+        //     try {
+        //         const {sub, exp} = jwtDecode(token)
+        //         if(sub.length > 0 && new Date(exp * 1000) > new Date()){
+        //             return navigate("/my-summary", {state: {info: "Session found, redirect to Summary Page"}})
+        //         }
+        //     } catch (e) {
+        //         queueSuccess("Invalid/ Expired token found, removing token", enqueueSnackbar)
+        //         Cookies.set("token", null);
+        //     }
+        // }
     }, [navigate]);
 
     return (
